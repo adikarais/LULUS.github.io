b4df2d006a10451b6e93c7dd844a559df9e19e31
AdikaRais <anandaadikarais200802@gmail.com>
2025-05-11 11:42:37 +0700

    Refactor search functionality and improve UI for search results

--- admin/search_admin.php
+++ admin/search_admin.php
@@ -1,20 +1,12 @@
 <?php
-
-// Menghubungkan file ini dengan file connect.php untuk koneksi ke database
 include '../components/connect.php';
 
-// Ensure $conn is defined and connected
-if (!isset($conn)) {
-    die("Database connection error.");
-}
-
-if(isset($_COOKIE['tutor_id'])){
+if (isset($_COOKIE['tutor_id'])) {
    $tutor_id = $_COOKIE['tutor_id'];
-}else{
-   $tutor_id = '';
+} else {
    header('location:login.php');
+   exit;
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -23,58 +15,59 @@ if(isset($_COOKIE['tutor_id'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>courses</title>
-
-   <!-- Menyertakan link ke Font Awesome untuk ikon -->
+   <title>Search</title>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-
-   <!-- Menyertakan file CSS kustom -->
-   <link rel="stylesheet" href="../css/style.css">
-
+   <link rel="stylesheet" href="../css/admin_style.css">
+   
 </head>
+<style>
+   .search-results .box-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 35rem);
+    gap: 1.5rem;
+    align-items: flex-start;
+    justify-content: center;
+   }
+   .search-results .box-container .box {
+    border-radius: 0.5rem;
+    background-color: var(--white);
+    padding: 2rem;
+   }
+</style>
 <body>
 
-<?php 
-// Menyertakan header pengguna
-include '../components/admin_header.php'; 
-?>
+<?php include '../components/admin_header.php'; ?>
 
-<!-- Bagian untuk menampilkan hasil pencarian dimulai -->
+<section class="search-results">
+   <h1 class="heading">Hasil Pencarian Lokasi</h1>
 
-<section class="courses">
-
-<h1 class="heading">search results</h1>
-
-<div class="box-container">
-
-   <?php
-      if(isset($_POST['search_course']) or isset($_POST['search_course_btn'])){
-         $search_course = isset($_POST['search_course']) ? $_POST['search_course'] : '';
+   <div class="box-container">
+      <?php
+      if(isset($_POST['search']) or isset($_POST['search_btn'])){
+         $search_course = $_POST['search'];
          $select_courses = $conn->prepare("SELECT * FROM `lokasi` WHERE nama_tempat LIKE :search_param");
          $search_param = "%{$search_course}%";
          $select_courses->bindValue(':search_param', $search_param, PDO::PARAM_STR);
          $select_courses->execute();
+         
          if($select_courses->rowCount() > 0){
             while($row = $select_courses->fetch(PDO::FETCH_ASSOC)){
-               echo "<div class='box' onclick=\"location.href='detail.php?id={$row['id']}'\">{$row['nama_tempat']}</div>";
+               echo "<div class='box' onclick=\"location.href='detail.php?id={$row['id']}'\">";
+               echo "<h3>{$row['nama_tempat']}</h3>";
+               echo "</div>";
             }
          } else {
-            echo "<div class='box'>No results found.</div>";
+            echo "<div class='box'>Tidak ada lokasi ditemukan!</div>";
          }
          $select_courses = null;
+      } else {
+         echo "<div class='box'>Silakan masukkan nama tempat yang ingin dicari!</div>";
       }
-   ?>
-
-</div>
-
+      ?>
+   </div>
 </section>
 
-<!-- Bagian untuk menampilkan hasil pencarian berakhir -->
-
 <?php include '../components/footer.php'; ?>
-
-<!-- Menyertakan file JavaScript kustom -->
-<script src="../js/script.js"></script>
-   
+<script src="../js/admin_script.js"></script>
 </body>
 </html>
